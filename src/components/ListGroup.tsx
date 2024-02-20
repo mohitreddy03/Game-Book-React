b60df85d95c7307863ae033f7267df72792d3982
mohitreddy03 <landu.mohit03@gmail.com>
2024-02-20 16:30:58 -0500

    Updated the list, made it dynamic

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,46 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+//import { MouseEvent } from "react";
+// {items : [], heading: string }
 
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  //let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  //let selectedIndex = 0;
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //const [name, setName] = useState('');
+  //items = [];
+
+  //const message =
+
+  //Event handler
+
+  //const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
