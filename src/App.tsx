021a2df47997860ab669ca77c1d31e6226fdbec2
mohitreddy03 <landu.mohit03@gmail.com>
2024-02-21 20:05:21 -0500

    added alters component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import ListGroup from "./components/ListGroup";
+//import ListGroup from "./components/ListGroup";
+
+import Alert from "./components/Alerts";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
